c5d3447c0bd284d7a7f821594b3860e5a9142320
esplanadeA <anna.li.dev@gmail.com>
2023-04-21 21:52:30 +1200

    components structure further developed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react'
 
-import { Article, Brand, CTA, Feature, Navbar } from './components'
+import { Brand, CTA, Navbar } from './components'
+import {Footer, Blog,Possibility,Features,WhatGPT,Header,} from './containers'
 
 const App = () => {
   return (
-    <div>
-      <h1>GPT-3</h1>
+    <div className='App'>
+      <div className='gradient__bg'>
+<Navbar></Navbar>
+<Header></Header>
+      </div>
+      <Brand/>
+      <WhatGPT/>
+      <Features/>
+      <Possibility/>
+      <CTA/>
+      <Blog/>
+      <Footer/>
     </div>
   )
 }
