7e32465f9d1084923940dfaec26a7ab82168b0da
Anna Li <128327012+esplanadeA@users.noreply.github.com>
2023-04-26 11:43:10 +1200

    Revert "Css style"

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import React from 'react'
+import logo from './logo.svg';
+import './App.css';
 
-import { Brand, CTA, Navbar } from './components'
-import {Footer, Blog,Possibility,Features,WhatGPT,Header,} from './containers'
-import './App.css'
-
-const App = () => {
+function App() {
   return (
-    <div className='App'>
-      <div className='gradient__bg'>
-         <Navbar></Navbar>
-         <Header></Header>
-      </div>
-      <Brand/>
-      <WhatGPT/>
-      <Features/>
-      <Possibility/>
-      <CTA/>
-      <Blog/>
-      <Footer/>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/brand/Brand.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import './brand.css'
-
-const Brand = () => {
-  return <div>brand</div>
-}
-
-export default Brand
